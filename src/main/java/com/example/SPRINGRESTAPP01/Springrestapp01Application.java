df80a42ea7c441c09cdc259ad1447fcd9e70ff6d
das-devi <das.angular@gmail.com>
2018-02-04 02:15:16 +0530

    added changes ib mail file

--- src/main/java/com/example/SPRINGRESTAPP01/Springrestapp01Application.java
+++ src/main/java/com/example/SPRINGRESTAPP01/Springrestapp01Application.java
@@ -8,6 +8,7 @@ public class Springrestapp01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springrestapp01Application.class, args);
+		System.out.println("HI this is my first change after git commit");
 	}
 	
 	//https://github.com/das-devi/SPRINGRESTAPP01.git
