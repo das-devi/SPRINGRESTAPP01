2c431510b21eb71c1eae88be47b3c6e73567943e
das-devi <das.angular@gmail.com>
2018-02-04 02:02:55 +0530

    my first git application changes

---
+++ src/main/java/com/example/SPRINGRESTAPP01/MyController.java
@@ -0,0 +1,15 @@
+package com.example.SPRINGRESTAPP01;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	@GetMapping("/greetings")
+	public String getGreetings() {
+		System.out.println("welcome to my first web application");
+		return "Welcome to my  First web application";
+	}
+
+}
---
+++ src/main/java/com/example/SPRINGRESTAPP01/Springrestapp01Application.java
@@ -0,0 +1,14 @@
+package com.example.SPRINGRESTAPP01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springrestapp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springrestapp01Application.class, args);
+	}
+	
+	//https://github.com/das-devi/SPRINGRESTAPP01.git
+}
